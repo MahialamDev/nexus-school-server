1cb802c6e0f55c285e6450ee8e9abbf7f4422a38
Mahialam Rahat <mahialam408@gmail.com>
2026-02-25 16:55:47 +0600

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,40 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@simplecrud.h04rjld.mongodb.net/?appName=SimpleCrud`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+let db;
+
+
+const connectDB = async function run() {
+  try {
+    await client.connect();
+    db = client.db('nexus-school');
+    console.log("Mongodb connected!");
+      
+
+  } catch (err) {
+      console.log(err)
+  }
+  
+  finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+
+
+// export
+const getDB = () => db;
+module.exports = { connectDB, getDB };
+
---
+++ src/routes/user.routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const { getDB } = require("../config/db"); // db.js path
+
+router.get("/", async (req, res) => {
+  try {
+    const db = getDB();
+    const users = await db.collection("users").find().toArray();
+    res.send(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+
+
+// insert user data
+
+router.post("/", async (req, res) => {
+  try {
+    const db = getDB();
+
+    const userInfo = req.body;
+    userInfo.role = "student";
+    userInfo.createdAt = new Date();
+
+    const result = await db.collection('users').insertOne(userInfo);
+      res.send(result);
+  } catch (err) {
+      console.log(err);
+  }
+});
+
+
+
+// export router
+module.exports = router;

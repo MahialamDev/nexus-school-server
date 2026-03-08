a5b48cbc01807df375620389c9370d941a4b2656
polokrf <polokkumar9030@gmail.com>
2026-03-08 09:30:24 +0600

    sucure routine instert

--- src/routes/routine.routes.js
+++ src/routes/routine.routes.js
@@ -23,6 +23,13 @@ router.post('/', async (req, res) => {
   try {
     const db = getDB();
     const routine = req.body;
+    new Date(routine.time);
+    const query = { day: routine.day, time: routine.time, period: routine.period, subject: routine.subject };
+    const findClass = await db.collection('routine').findOne({ query });
+    if (findClass) {
+      return res.send({message:'already create routine plz check and only update now'})
+    }
+
     const result = await db.collection('routine').insertOne(routine);
     res.send(result);
   } catch (error) {

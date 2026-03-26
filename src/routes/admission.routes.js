a68208f558ea663f29fe2417e6e4f1de68029b12
Mahialam Rahat <mahialam408@gmail.com>
2026-03-26 15:03:46 +0600

    update admition patch

--- src/routes/admission.routes.js
+++ src/routes/admission.routes.js
@@ -1,52 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getDB } = require('../config/db');
-const { ObjectId } = require('mongodb');
+const { getDB } = require("../config/db");
+const { ObjectId } = require("mongodb");
 const { generateUniqueStudentId } = require("../utils/generateStudentId");
 const generateStudentRoll = require("../utils/generateStudentRoll");
 
 // post method for admission application
-router.post('/', async (req, res) => {
-    try {
-        const db = getDB();
-        if (!db) return res.status(500).send('DB not connected');
-        const applicationInfo = req.body;
-        const name = await applicationInfo.firstName + ' ' + applicationInfo.lastName;
-
-        const applicationData = {
-            name,
-            ...req.body,
-            application_status: 'pending', 
-            submittedAt: new Date()
-        };
-
-        const result = await db.collection('admissions').insertOne(applicationData);
-        // insertOne for frontend saving data in database and send response to frontend
-        res.status(201).send({ success: true, ...result }); 
-    } catch (error) {
-        res.status(400).send({ success: false, message: "Failed", error: error.message });
+router.post("/", async (req, res) => {
+  try {
+    const db = getDB();
+    if (!db) return res.status(500).send("DB not connected");
+    const applicationInfo = req.body;
+     // Check duplicate email
+    const existingEmail = await db.collection('admissions').findOne({ email: applicationInfo.email });
+    if (existingEmail) {
+      return res.status(409).send({ success: false, message: "Already applied" });
     }
+
+    const name = await applicationInfo.firstName + " " + applicationInfo.lastName;
+
+    const applicationData = {
+      name,
+      ...req.body,
+      application_status: "pending",
+      submittedAt: new Date(),
+    };
+
+    const result = await db.collection("admissions").insertOne(applicationData);
+    // insertOne for frontend saving data in database and send response to frontend
+    res.status(201).send({ success: true, ...result });
+  } catch (error) {
+    res
+      .status(400)
+      .send({ success: false, message: "Failed", error: error.message });
+  }
 });
 
 // GET method for fetching all applications (Admin only)
-router.get('/', async (req, res) => {
-    try {
-        const db = getDB();
-        if (!db) return res.status(500).send('DB not connected');
+router.get("/", async (req, res) => {
+  try {
+    const db = getDB();
+    if (!db) return res.status(500).send("DB not connected");
 
-        const applications = await db.collection('admissions')
-            .find()
-            .sort({ submittedAt: -1 })
-            .toArray();
+    const applications = await db
+      .collection("admissions")
+      .find()
+      .sort({ submittedAt: -1 })
+      .toArray();
 
-        res.status(200).send(applications); 
-    } catch (error) {
-        res.status(500).send({ message: "Server Error", error: error.message });
-    }
+    res.status(200).send(applications);
+  } catch (error) {
+    res.status(500).send({ message: "Server Error", error: error.message });
+  }
 });
 
 // PATCH method for updating application status (Admin only)
-router.patch('/:id', async (req, res) => {
+router.patch("/:id", async (req, res) => {
   try {
     const db = getDB();
     const { id } = req.params;
@@ -62,6 +71,11 @@ router.patch('/:id', async (req, res) => {
       return res.status(404).send({ message: "Admission not found" });
     }
 
+    const existStudent = await db.collection("students").findOne({
+      admission_id: studentInfo._id,
+      email: studentInfo.email,
+    });
+
     const className = studentInfo.class;
     const year = new Date().getFullYear();
 
@@ -74,7 +88,7 @@ router.patch('/:id', async (req, res) => {
         db,
         studentInfo.name,
         className,
-        year
+        year,
       );
 
       roll = await generateStudentRoll(db, className, year);
@@ -90,7 +104,52 @@ router.patch('/:id', async (req, res) => {
       },
     };
 
-    const result = await db.collection("admissions").updateOne(filter, updateDoc);
+    const result = await db
+      .collection("admissions")
+      .updateOne(filter, updateDoc);
+
+    if (!existStudent && status.toLowerCase() === "approved") {
+      await db.collection("students").insertOne({
+        admission_id: studentInfo._id,
+        name: studentInfo.name,
+        email: studentInfo.email,
+        class: className,
+        phone: studentInfo.phone || "Not Set",
+        address: studentInfo.address || "Not Set",
+        student_id,
+        roll,
+        enrollment_status: "enrolled",
+        academic_year: year,
+        createdAt: new Date(),
+      });
+
+      const user = await db
+        .collection("users")
+        .findOne({ email: studentInfo.email });
+
+      if (!user) {
+        // Create a new user
+        await db.collection("users").insertOne({
+          name: studentInfo.name,
+          email: studentInfo.email,
+          phone: studentInfo.phone || "Not Set",
+          address: studentInfo.address || "Not Set",
+          role: "student", // default role
+          status: "active",
+          createdAt: new Date(),
+        });
+      } else {
+        // Just update role if needed
+        if (user.role !== "student") {
+          await db
+            .collection("users")
+            .updateOne(
+              { email: studentInfo.email },
+              { $set: { role: "student" } },
+            );
+        }
+      }
+    }
 
     res.status(200).send(result);
   } catch (error) {
@@ -98,20 +157,19 @@ router.patch('/:id', async (req, res) => {
   }
 });
 
-
 //  Delete a specific application
-router.delete('/:id', async (req, res) => {
-    try {
-        const db = getDB();
-        const id = req.params.id;
-        if (!db) return res.status(500).send('DB not connected');
+router.delete("/:id", async (req, res) => {
+  try {
+    const db = getDB();
+    const id = req.params.id;
+    if (!db) return res.status(500).send("DB not connected");
 
-        const query = { _id: new ObjectId(id) };
-        const result = await db.collection('admissions').deleteOne(query);
-        res.status(200).send(result);
-    } catch (error) {
-        res.status(500).send({ message: "Error deleting", error: error.message });
-    }
+    const query = { _id: new ObjectId(id) };
+    const result = await db.collection("admissions").deleteOne(query);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send({ message: "Error deleting", error: error.message });
+  }
 });
 
 module.exports = router;

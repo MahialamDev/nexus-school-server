d334e73e6faf1ab376a23b4fc56b0ea754965e2a
Mahialam Rahat <mahialam408@gmail.com>
2026-03-30 18:38:58 +0600

    remove payment routers

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const studentsRouter =require('./routes/students.routes')
 const notifications =require('./routes/notification.routes')
 const studentRoutes = require("./routes/studentRoutes");
 const temp = require("./routes/tempory.routes");
-const paymentRoutes = require('./routes/payments.routes')
+
 
 // middleware
 app.use(express.json());
@@ -72,8 +72,6 @@ app.use('/notifications',notifications)
 // notification route
 app.use('/temp', temp)
 
-// payments
-app.use('/payments', paymentRoutes )
 
 
 
